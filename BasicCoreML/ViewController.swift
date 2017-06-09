0108d63e48ae37ac14fc64a207f10dea50a27aaf
Brian Advent <brianadvent@brians-macbook-pro.local>
2017-06-09 21:50:38 +0200

    Core ML Introduction

--- BasicCoreML/ViewController.swift
+++ BasicCoreML/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    let model = GoogLeNetPlaces()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        let imageView = sender.view as? UIImageView
+        
+        if let imageToAnalyse = imageView?.image {
+            if let sceneLabelString = sceneLabel(forImage: imageToAnalyse) {
+                categoryLabel.text = sceneLabelString
+            }
+        }
+        
+        
+    }
+    
+    func sceneLabel (forImage image:UIImage) -> String? {
+        if let pixelBuffer = ImageProcessor.pixelBuffer(forImage: image.cgImage!) {
+            guard let scene = try? model.prediction(sceneImage: pixelBuffer) else {fatalError("Unexpected runtime error")}
+            return scene.sceneLabel
+            
+        }
+        
+        return nil
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
